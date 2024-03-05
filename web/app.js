fa002dbd06a57ea81a7792ccd0bb3afec930e3fc
jessicamalejcikova <jmalejci@iu.edu>
2024-03-05 18:56:33 +0000

    everything worksgit add .

--- web/app.js
+++ web/app.js
@@ -9,11 +9,17 @@ const {Storage} = require('@google-cloud/storage');
 
   // Create instances of necessary packages
   const app = express();
+  const storage = new Storage();
 
   // Set the identifier for the GCS bucket where the file will be stored
-
+  const bucket = storage.bucket('sp24-41200-malejci-gj-uploads');
   // Configure an instance of multer
-
+  const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+      fileSize: 15 * 1024 * 1024
+    }
+  })
   // Middleware
   app.use('/js', express.static(__dirname + '/public/js'));
   app.use('/css', express.static(__dirname + '/public/css'));
@@ -25,8 +31,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.post('/upload', (req, res) => {
-  res.redirect('/');
+app.post('/upload', multer.single('file'), (req, res) => {
+  //Ensure a file was uploaded from the form
+  //If not, respond with a simple message and return from this function
+  if(!req.file) {
+    res.status(400).send('No file uploaded');
+    return;
+  }
+
+  //This will run if a file is uploaded
+  //Create a streaming target where the file will be stored in GCS
+  const blob = bucket.file(req.file.originalname);
+
+  //Begin streaming the file to GCS
+  const blobStream = blob.createWriteStream();
+
+  //When the file has uploaded, redirect the browser to the home page
+  blobStream.on('finish',() => {
+    const uploadedFile = `${bucket.name}/${blob.name}`;
+    console.log(`File uploaded: ${uploadedFile}`);
+    res.redirect('/');
+  });
+    //Close the stream
+    blobStream.end(req.file.buffer);
 });
 
 app.listen(port, () => {
